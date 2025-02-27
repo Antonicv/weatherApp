7939d7054f4c049be7844d05040feb332e81e7da
Toni <antonicv.88@gmail.com>
2025-02-27 10:17:28 +0100

    fix: update Navbar background color and restructure App component with Router

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ display: "flex", gap: "15px", padding: "10px", background: "#eee" }}>
+    <nav style={{ display: "flex", gap: "15px", padding: "10px", background: "#eeeeee" }}>
       <Link to="/"> Home</Link>
       <Link to="/weather"> Weather</Link>
       <Link to="/map">Map</Link>
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -3,21 +3,22 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./home.jsx";
 import Map from "./map.jsx";
 import About from "./about.jsx";
-import Weather from './Weather';
-import Navbar from "./NavBar.jsx"; 
-
+import Navbar from "../components/Navbar";
+import "../styles/index.css"
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/weather" element={<Weather />} />
-        <Route path="/map" element={<Map />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/weather" element={<Weather />} />
+          <Route path="/map" element={<Map />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
